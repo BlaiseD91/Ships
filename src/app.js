f01eabb5b44efb186fdb281980e53e37542d5c49
Székely Balázs Csaba <szekelyb91@gmail.com>
2023-03-22 10:05:11 +0100

    Kezdés, feladatok meghatározása

---
+++ src/app.js
@@ -0,0 +1,12 @@
+/*
+* File: app.js
+* Author: Székely Balázs Csaba
+* Copyright: 2023, Székely Balázs Csaba
+* Group: SZOFT I/1/E
+* Date: 2023-03-22
+* Github: https://github.com/BlaiseD91
+* Licenc: GNU GPL
+*/
+
+// TODO: Az adatokat JavaScript fetch() utasítással kérje le a REST API-ból, majd tárolja tömbben.
+// TODO: A tömböt a weblapon egy táblázatba kell generálnia.
