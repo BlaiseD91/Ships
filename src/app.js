40fde313348b78b3fda0374435bf4c81c53aa77f
Székely Balázs Csaba <szekelyb91@gmail.com>
2023-03-24 20:27:39 +0100

    adatok eltárolása kész

--- src/app.js
+++ src/app.js
@@ -8,5 +8,34 @@
 * Licenc: GNU GPL
 */
 
-// TODO: Az adatokat JavaScript fetch() utasítással kérje le a REST API-ból, majd tárolja tömbben.
+const state = {
+    ships: []
+};
+
+const doc = {
+    shipsTableBody: null
+}
+
+window.addEventListener('load', () => {
+    init();
+    loadShips();
+});
+
+function init(){
+    doc.shipsTableBody = document.querySelector('.shipsTableBody');
+}
+
+function loadShips(){
+    let url = "http://localhost:8000/";
+    let endpoint = "ships";
+    let path = url + endpoint;
+    fetch(path)
+    .then(response => response.json())
+    .then(result => {
+        state.ships = result;
+        console.log(result);
+    });
+}
+
+
 // TODO: A tömböt a weblapon egy táblázatba kell generálnia.
